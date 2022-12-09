19da2481967cf9e03ccbb7a6a60ad0a4675d7fb2
Midyanisa Yuniar <mnynoname14@gmail.com>
2022-12-10 02:29:24 +0700

    14 make links (#19)
    * links: popup edit
    
    * links: add default link & get link
    
    * links: button delete'
    
    * links: delete link & button edit
    
    * links: edit link
    
    * links: delete button
    
    * links: custom link
    
    * add logout
    
    * links: fix bug create link
    
    * orderBy created_at
    
    * orderBy created_at

---
+++ frontend/src/components/PopupMsg.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    class="fixed z-10 flex items-center justify-center w-full h-full mr-5"
+    :class="{
+      hidden:
+        app.links.success == false &&
+        app.links.delete == false &&
+        app.links.edit == false,
+    }"
+  >
+    <div
+      class="z-20 flex items-center justify-center w-full h-full mr-5 bg-black opacity-60 my-7"
+      @click="
+        (app.links.success = false),
+          (app.links.edit = false),
+          (app.links.delete = false),
+          (app.links.path = ``)
+      "
+    ></div>
+    <SuccessMsg v-if="app.links.success" :copy="copy" />
+    <EditMsg v-if="app.links.edit" :id="id" />
+    <DeleteMsg v-if="app.links.delete" :id="id" />
+  </div>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue";
+import { useApp } from "../stores/index.js";
+import SuccessMsg from "./SuccessMsg.vue";
+import EditMsg from "./EditMsg.vue";
+import DeleteMsg from "./DeleteMsg.vue";
+
+export default {
+  props: {
+    copy: {
+      type: Boolean,
+      default: false,
+    },
+    id: {
+      type: String,
+      default: "",
+    },
+  },
+  components: {
+    Icon,
+    SuccessMsg,
+    EditMsg,
+    DeleteMsg,
+  },
+  setup() {
+    const app = useApp();
+    return {
+      app,
+    };
+  },
+};
+</script>
